c1a6e7317c446052cd4de980b7033f210aff34f8
Maxime Boissonneault <maxime.boissonneault@calculquebec.ca>
2025-11-24 11:14:20 -0500

    fix comments in jsx

--- src/account-settings/JumpNav.jsx
+++ src/account-settings/JumpNav.jsx
@@ -22,11 +22,11 @@ const JumpNav = ({
       <Scrollspy
         items={[
           'basic-information',
-//          'profile-information',
-//          'social-media',
+/*          'profile-information',*/
+/*          'social-media',*/
           'site-preferences',
-//          'linked-accounts',
-//          'delete-account',
+/*          'linked-accounts',*/
+/*          'delete-account',*/
         ]}
         className="list-unstyled"
         currentClassName="font-weight-bold"
@@ -36,34 +36,35 @@ const JumpNav = ({
             {intl.formatMessage(messages['account.settings.section.account.information'])}
           </NavHashLink>
         </li>
-//        <li>
-//          <NavHashLink to="#profile-information">
-//            {intl.formatMessage(messages['account.settings.section.profile.information'])}
-//          </NavHashLink>
-//        </li>
-//        <li>
-//          <NavHashLink to="#social-media">
-//            {intl.formatMessage(messages['account.settings.section.social.media'])}
-//          </NavHashLink>
-//        </li>
+{/*
+        <li>
+          <NavHashLink to="#profile-information">
+            {intl.formatMessage(messages['account.settings.section.profile.information'])}
+          </NavHashLink>
+        </li>
+        <li>
+          <NavHashLink to="#social-media">
+            {intl.formatMessage(messages['account.settings.section.social.media'])}
+          </NavHashLink>
+        </li>*/}
         <li>
           <NavHashLink to="#site-preferences">
             {intl.formatMessage(messages['account.settings.section.site.preferences'])}
           </NavHashLink>
         </li>
-//        <li>
-//          <NavHashLink to="#linked-accounts">
-//            {intl.formatMessage(messages['account.settings.section.linked.accounts'])}
-//          </NavHashLink>
-//        </li>
-//        {getConfig().ENABLE_ACCOUNT_DELETION
-//          && (
-//          <li>
-//            <NavHashLink to="#delete-account">
-//              {intl.formatMessage(messages['account.settings.jump.nav.delete.account'])}
-//            </NavHashLink>
-//          </li>
-//          )}
+{/*        <li>
+          <NavHashLink to="#linked-accounts">
+            {intl.formatMessage(messages['account.settings.section.linked.accounts'])}
+          </NavHashLink>
+        </li>
+        {getConfig().ENABLE_ACCOUNT_DELETION
+          && (
+          <li>
+            <NavHashLink to="#delete-account">
+              {intl.formatMessage(messages['account.settings.jump.nav.delete.account'])}
+            </NavHashLink>
+          </li>
+          )}*/}
       </Scrollspy>
       {showPreferences && (
         <>
